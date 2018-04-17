59d8b126a63c37f75d240d023411fc5a3918c5e1
hacktiv8 <hacktiv8@H8JKT034.localdomain>
2018-04-17 15:12:17 +0700

    Change Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Yeay Bisa Travis Ci' });
 });
 
 module.exports = router;
